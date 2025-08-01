f9d6bd376c5e3ad073ddce31d5bfdfce67774896
Rizky Alam <sketsacartoon@gmail.com>
2025-08-01 09:49:02 +0700

    fix: port vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5174
-  },
   preview: {
-    host: '0.0.0.0',
-    port: 3000
-  }
-})
+    host: "0.0.0.0",
+    port: 3000,
+    allowedHosts: ["publisher.ithbi.com"],
+  },
+});
